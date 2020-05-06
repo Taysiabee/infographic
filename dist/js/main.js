ec58b74c1c42e69d45e0c2b3f65d25f76bbdd257
Taysiabee <50152275+Taysiabee@users.noreply.github.com>
2020-05-06 15:03:30 -0400

    media queries

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
-var elem = document.querySelector('.pills');
-window.addEventListener('scroll', function () {
-  var value = window.scrollY * 0.25;
-  elem.style.transform = `translatex(-50%) translatey(-50%) rotate(${value}deg)`;
-});
+// var elem = document.querySelector('.pills');
+// window.addEventListener('scroll', function() {
+// 	var value = window.scrollY * 0.25;
+// 	elem.style.transform = `translatex(-50%) translatey(-50%) rotate(${value}deg)`; 
+// });
 //# sourceMappingURL=main.js.map
