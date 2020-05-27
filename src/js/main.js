dc698da1bfe773102b4670c1986612632b027204
Taysiabee <50152275+Taysiabee@users.noreply.github.com>
2020-05-27 15:33:27 -0400

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,34 @@
-// var elem = document.querySelector('.pills');
-// window.addEventListener('scroll', function() {
-// 	var value = window.scrollY * 0.25;
-// 	elem.style.transform = `translatex(-50%) translatey(-50%) rotate(${value}deg)`; 
-// });
+
+window.onload = function() {
+	var testWaypoint = new Waypoint({
+		element: document.querySelector('.america-spends'),
+		handler: function(direction) {
+			console.log('bottom')
+	  },
+	  offset: 'bottom-in-view'
+	});
+
+	var moneyPileWaypoint = new Waypoint({
+		element: document.querySelector('.america-spends'),
+		handler: function(direction) {
+			document.querySelector(".moneyone").classList.add("uk-drops1");
+			document.querySelector(".moneytwo").classList.add("uk-drops2");
+			document.querySelector(".moneythree").classList.add("us-drops1");
+			document.querySelector(".moneyfour").classList.add("us-drops2");
+			document.querySelector(".moneyfive").classList.add("us-drops3");
+			document.querySelector(".moneysix").classList.add("us-drops4");
+	  },
+	  offset: 'bottom-in-view'
+	});
+
+	// var graphLineWaypoint = new Waypoint({
+	// 	element: document.querySelector('.graph'),
+	// 	handler: function(direction) {
+	// 		console.log("6513")
+	// 		document.querySelector(".red-line-cover-box").classList.add("animate")
+			
+	//   },
+	//   offset: 'bottom-in-view'
+	// });
+}
+
